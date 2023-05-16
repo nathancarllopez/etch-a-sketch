c2d272fd6448f1cff25116bb1b49348f5f9191d2
Nathan Lopez <nathancarllopez@gmail.com>
2023-05-16 11:32:46 -0500

    added css
    added button to set grid size
    
    changed style of grid squares

--- etchASketch.js
+++ etchASketch.js
@@ -1,12 +1,42 @@
-GRIDSIZE = 16;
-
-/** Add grid of divs to #grid-container div */
-const gridContainer = document.querySelector('#grid-container');
-for (let _ = 0; _ < GRIDSIZE; _++) {
-    const div = document.createElement('div');
-    div.setAttribute('id', 'grid-square');
-    const label = document.createElement('p');
-    label.textContent = `${_ + 1}`;
-    div.appendChild(label);
-    gridContainer.appendChild(div);
+function removeOldGrid() {
+    /** Remove the old grid */
+    const gridContainer = document.querySelector('#grid-container');
+    const allGridSquares = document.querySelectorAll('#grid-square');
+    allGridSquares.forEach((gridSquare) => {
+        gridContainer.removeChild(gridSquare);
+    })
 }
+
+function fillSquare() {
+
+}
+
+function createNewGrid() {
+    /** Ask user for grid size */
+    let size = prompt('Enter a number between 1-14 to set the grid size.');
+    while (size > 14) {
+        alert("Too large a size, try again.");
+        size = prompt('Enter a number between 1-14 to set the grid size.');
+    }
+    
+    /** Add grid of divs to #grid-container div */
+    const gridContainer = document.querySelector('#grid-container');
+    gridContainer.setAttribute('style', `grid-template-columns: repeat(${size}, 50px)`)
+    for (let _ = 0; _ < size * size; _++) {
+        /** Create #grid-square */
+        const gridSquare = document.createElement('div');
+        gridSquare.setAttribute('id', 'grid-square');
+
+        /** Add event listener for hover effect */
+        
+
+        /** Add #grid-square to #grid-container */
+        gridContainer.appendChild(gridSquare);
+    }
+}
+
+const setSizeButton = document.querySelector('#set-size');
+setSizeButton.addEventListener('click', () => {
+    removeOldGrid();
+    createNewGrid();
+});
